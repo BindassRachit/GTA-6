e0fa23e6afc232e10e59459dbeb39b1a2193ecf0
Rachit Gupta <rachitgupta98977@gmail.com>
2025-07-25 01:01:49 +0530

    contact fully code change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,38 +1,31 @@
 import AnimatedTitle from "./AnimatedTitle";
 import Button from "./Button";
 
-const ImageClipBox = ({ src, clipClass }) => (
-  <div className={clipClass}>
-    <img src={src} />
-  </div>
-);
-
 const Contact = () => {
   return (
-    <div id="contact" className="my-20 min-h-96 w-screen  px-10">
+    <div id="contact" className="my-20 min-h-96 w-screen px-10">
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+        {/* Left Side Images */}
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
-          <ImageClipBox
-            src="/GTA-6/img/contact-1.webp"
-            clipClass="contact-clip-path-1"
-          />
-          <ImageClipBox
-            src="/GTA-6/img/contact-2.webp"
-            clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
-          />
+          <div className="contact-clip-path-1">
+            <img src="img/contact-1.webp" alt="contact-1" />
+          </div>
+          <div className="contact-clip-path-2 lg:translate-y-40 translate-y-60">
+            <img src="img/contact-2.webp" alt="contact-2" />
+          </div>
         </div>
 
+        {/* Swordman Image */}
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
-          <ImageClipBox
-            src="img/swordman.png"
-            clipClass="absolute md:scale-125"
-          />
-          <ImageClipBox
-            src="img/swordman.png"
-            clipClass="sword-man-clip-path md:scale-125"
-          />
+          <div className="absolute md:scale-125">
+            <img src="img/swordman.png" alt="swordman" />
+          </div>
+          <div className="sword-man-clip-path md:scale-125">
+            <img src="img/swordman.png" alt="swordman clipped" />
+          </div>
         </div>
 
+        {/* Text Content */}
         <div className="flex flex-col items-center text-center">
           <p className="mb-10 font-general text-[10px] uppercase">
             Rachit Gupta
